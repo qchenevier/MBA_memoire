d830509bd8688ff4a1f0a951d3b451269ef887da
qchenevier <qchenevier@users.noreply.github.com>
2017-07-22 12:17:03 +0200

    added citation support with example files

--- doc/md2pdf.sh
+++ doc/md2pdf.sh
@@ -1,8 +1,9 @@
 
 FILE=$1
-FONT=${2:-"DejaVu Sans"}
+FONT=${2:-"Garuda"}
+BIB=${3:-"mendeley_export"}
+CSL=${4:-"apa"}
 
-command="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE".md --latex-engine=xelatex --toc -o "$FILE".pdf"
-eval $command
+command="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE".md --latex-engine=xelatex --toc --filter=pandoc-citeproc --biblio="$BIB".bib --csl="$CSL".csl -o "$FILE".pdf"
 
-evince "$FILE".pdf
+eval $command && evince "$FILE".pdf
