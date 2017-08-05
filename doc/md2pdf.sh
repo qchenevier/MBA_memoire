1e57ecafc6b5a257e1c7e57427a9bc4897361584
qchenevier <qchenevier@users.noreply.github.com>
2017-08-05 15:54:29 +0200

    Added "top-level-division=chapter" option

--- doc/md2pdf.sh
+++ doc/md2pdf.sh
@@ -4,7 +4,7 @@ FONT=${2:-"FreeSans"}
 BIB=${3:-"/home/quentin/documents/bibliography/library"}
 CSL=${4:-"apa"}
 
-generate_report="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE"*.md --latex-engine=xelatex --toc --filter=pandoc-include --filter=pandoc-citeproc --biblio="$BIB".bib --csl="$CSL".csl --tab-stop=2 -o "$FILE".pdf"
+generate_report="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE"*.md --latex-engine=xelatex --toc --filter=pandoc-include --filter=pandoc-citeproc --biblio="$BIB".bib --csl="$CSL".csl --tab-stop=2 --top-level-division=chapter -o "$FILE".pdf"
 
 launch_pdf_reader="pkill -f 'evince \""$FILE"\".pdf'; evince \""$FILE"\".pdf >/dev/null 2>&1 &"
 
