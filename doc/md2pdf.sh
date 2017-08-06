f9c23a2718a69398a7735664b3fa235fac052875
qchenevier <qchenevier@users.noreply.github.com>
2017-08-06 10:50:31 +0200

    improved pdf viewing: no more kill then reopen just bring the app on the front

--- doc/md2pdf.sh
+++ doc/md2pdf.sh
@@ -6,6 +6,13 @@ CSL=${4:-"apa"}
 
 generate_report="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE"*.md --latex-engine=xelatex --toc --filter=pandoc-include --filter=pandoc-citeproc --biblio="$BIB".bib --csl="$CSL".csl --tab-stop=2 --top-level-division=chapter -o "$FILE".pdf"
 
-launch_pdf_reader="pkill -f 'qpdfview \""$FILE"\".pdf'; qpdfview \""$FILE"\".pdf >/dev/null 2>&1 &"
-
-eval $generate_report && eval $launch_pdf_reader
+if eval $generate_report; then
+  if ! pgrep -f "qpdfview "$FILE".pdf"; then
+    echo process not found
+    qpdfview "$FILE".pdf &
+  else
+    echo process found
+    qpdfview_pid=`pgrep -f "qpdfview "$FILE".pdf"`
+    xdotool windowactivate `xdotool search --pid "$qpdfview_pid" | tail -1`
+  fi
+fi
