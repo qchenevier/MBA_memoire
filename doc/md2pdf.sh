907fbf03087c351c687043474d6d8434949f3005
qchenevier <qchenevier@users.noreply.github.com>
2017-08-06 09:59:50 +0200

    use of qpdfview to view pdf: evince was lagging

--- doc/md2pdf.sh
+++ doc/md2pdf.sh
@@ -6,6 +6,6 @@ CSL=${4:-"apa"}
 
 generate_report="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE"*.md --latex-engine=xelatex --toc --filter=pandoc-include --filter=pandoc-citeproc --biblio="$BIB".bib --csl="$CSL".csl --tab-stop=2 --top-level-division=chapter -o "$FILE".pdf"
 
-launch_pdf_reader="pkill -f 'evince \""$FILE"\".pdf'; evince \""$FILE"\".pdf >/dev/null 2>&1 &"
+launch_pdf_reader="pkill -f 'qpdfview \""$FILE"\".pdf'; qpdfview \""$FILE"\".pdf >/dev/null 2>&1 &"
 
 eval $generate_report && eval $launch_pdf_reader
