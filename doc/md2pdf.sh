dc01371f51dd74aacb16a5237294fb19677a0b3e
qchenevier <qchenevier@users.noreply.github.com>
2017-09-20 06:29:36 +0200

    created bash script to replace gulp

--- doc/md2pdf.sh
+++ doc/md2pdf.sh
@@ -6,13 +6,15 @@ CSL=${4:-"apa"}
 
 generate_report="pandoc -N --template=mytemplate.tex --variable mainfont=\""$FONT"\" --variable sansfont=\""$FONT"\" --variable monofont=\"DejaVu Sans Mono\" --variable fontsize=12pt --variable version=1.17.2 "$FILE"*.md --latex-engine=xelatex --toc --filter=pandoc-include --filter=pandoc-citeproc --biblio="$BIB".bib --csl="$CSL".csl --tab-stop=2 --top-level-division=chapter -o "$FILE".pdf"
 
+echo  $(date -Iseconds): generating pdf: $FILE
+
 if eval $generate_report; then
-  if ! pgrep -f "qpdfview "$FILE".pdf"; then
-    echo qpdfview process not found
+  qpdfview_pid=`pgrep -f "qpdfview "$FILE".pdf"`
+  if [ -z "$qpdfview_pid" ]; then
+    echo  $(date -Iseconds): opening in new qpdfview
     qpdfview "$FILE".pdf &
   else
-    echo qpdfview process found
-    qpdfview_pid=`pgrep -f "qpdfview "$FILE".pdf"`
+    echo  $(date -Iseconds): opening in existing qpdfview with pid: $qpdfview_pid
     xdotool windowactivate `xdotool search --pid "$qpdfview_pid" | tail -1`
   fi
 fi
